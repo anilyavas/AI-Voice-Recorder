7be9f220f0deba89a4c92fe1e67f228de7afdcb4
anilyavas <anil.yavas99@gmail.com>
2024-11-20 18:44:32 +0300

    transcribe error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,50 +3,32 @@ import { useRouter } from 'expo-router';
 import { Stack } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Toaster } from 'sonner-native';
-import { PostHogProvider } from 'posthog-react-native';
-
-const POSTHOG_API_KEY = process.env.EXPO_PUBLIC_POSTHOG_API_KEY;
-const HOST = process.env.EXPO_PUBLIC_POSTHOG_HOST;
-if (!POSTHOG_API_KEY) {
-  throw new Error(
-    'Missing PostHog API Key. Please set EXPO_PUBLIC_POSTHOG_API_KEY in your .env'
-  );
-}
 
 export default function RootLayout() {
   const router = useRouter();
   return (
     <GestureHandlerRootView>
-      <PostHogProvider
-        apiKey={POSTHOG_API_KEY}
-        debug={true}
-        options={{
-          host: HOST,
-          enableSessionReplay: true,
-        }}
-      >
-        <Toaster />
-        <Stack>
-          <Stack.Screen name='index' options={{ title: 'Voice Recorder' }} />
-          <Stack.Screen
-            name='new-recording'
-            options={{
-              title: 'New Recording',
-              presentation: 'modal',
-              headerLeft: () => {
-                return (
-                  <Ionicons
-                    name='close'
-                    size={24}
-                    color='black'
-                    onPress={() => router.back()}
-                  />
-                );
-              },
-            }}
-          />
-        </Stack>
-      </PostHogProvider>
+      <Toaster />
+      <Stack>
+        <Stack.Screen name='index' options={{ title: 'Voice Recorder' }} />
+        <Stack.Screen
+          name='new-recording'
+          options={{
+            title: 'New Recording',
+            presentation: 'modal',
+            headerLeft: () => {
+              return (
+                <Ionicons
+                  name='close'
+                  size={24}
+                  color='black'
+                  onPress={() => router.back()}
+                />
+              );
+            },
+          }}
+        />
+      </Stack>
     </GestureHandlerRootView>
   );
 }
--- app/new-recording.tsx
+++ app/new-recording.tsx
@@ -1,8 +1,43 @@
-import { useLocalSearchParams } from 'expo-router';
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { toast } from 'sonner-native';
 
 export default function Page() {
   const { uri } = useLocalSearchParams<{ uri?: string }>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [transcription, setTranscription] = useState('');
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!uri) return;
+    handleTranscribe();
+  }, [uri]);
+
+  const handleTranscribe = async () => {
+    setIsLoading(true);
+    toast.loading('Transcribing...');
+    try {
+      const formData = new FormData();
+      const audioData = { uri, name: 'audio.m4a', type: 'audio/m4a' };
+      formData.append('file', audioData as unknown as Blob);
+
+      const response = await fetch('/api/speech-to-text', {
+        method: 'POST',
+        headers: { 'Content-Type': 'multipart/form-data' },
+        body: formData,
+      }).then((res) => res.json());
+      console.log(response);
+      setTranscription(response.text);
+    } catch (error) {
+      console.log(error);
+      toast.error('Failed to transcribe audio');
+    } finally {
+      setIsLoading(false);
+      toast.dismiss();
+    }
+  };
+
   return (
     <View>
       <Text>Page</Text>
